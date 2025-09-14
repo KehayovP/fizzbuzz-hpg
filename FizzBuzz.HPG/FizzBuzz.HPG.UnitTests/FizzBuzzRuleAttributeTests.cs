02b91e661737f225a7d020a2ae4da113ff65d363
Petar <kehayovp@outlook.com>
2025-09-14 16:19:50 +0100

    test: non positive divisors are not accepted in the attribute

---
+++ FizzBuzz.HPG/FizzBuzz.HPG.UnitTests/FizzBuzzRuleAttributeTests.cs
@@ -0,0 +1,12 @@
+namespace FizzBuzz.HPG.UnitTests;
+
+public class FizzBuzzRuleAttributeTests
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Ctor_WithNonPositiveDivisor_Throws(int divisor)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new FizzBuzzRuleAttribute(divisor, "X", 0));
+    }
+}
